1524a5b2b8ac56d22bdad05d3f374723dca22588
Diva <divaliu1408@qq.com>
2018-08-29 16:57:20 +0800

    增加后天自动更新天气的功能

--- app/src/main/java/com/diva/coolweather/WeatherActivity.java
+++ app/src/main/java/com/diva/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.diva.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.diva.coolweather.gson.Forecast;
 import com.diva.coolweather.gson.Weather;
+import com.diva.coolweather.service.AutoUpdateService;
 import com.diva.coolweather.util.HttpUtil;
 import com.diva.coolweather.util.Utility;
 
@@ -219,6 +221,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
